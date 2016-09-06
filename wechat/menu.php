c0a3b374a3a4fa1c864a73ac3035f1b23c1d6415
breezi30 <breezi30@gmail.com>
2016-09-06 23:15:23 +0800

    chang the http index

--- wechat/menu.php
+++ wechat/menu.php
@@ -12,7 +12,7 @@
                 array('id'=>'1', 'pid'=>'0', 'name'=>'分享推广', 'type'=>'', 'code'=>''),
                 array('id'=>'2', 'pid'=>'1', 'name'=>'分享二维码', 'type'=>'click', 'code'=>'key_1'),
                 array('id'=>'3', 'pid'=>'1', 'name'=>'分享好文章', 'type'=>'click', 'code'=>'key_2'),
-                array('id'=>'4', 'pid'=>'0', 'name'=>'我的统计', 'type'=>'view', 'code'=>'http://www.giant-land.com/zhuye.html'),
+                array('id'=>'4', 'pid'=>'0', 'name'=>'我的统计', 'type'=>'view', 'code'=>'http://www.giant-land.com/index.html'),
                 
             );
 
